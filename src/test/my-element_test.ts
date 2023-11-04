505949bc5b322086f998c1e4d0bfe5736bef8081
Redish101 <64068446+Redish101@users.noreply.github.com>
2023-11-04 09:13:35 +0800

    Initial commit

---
+++ src/test/my-element_test.ts
@@ -0,0 +1,71 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {MyElement} from '../my-element.js';
+
+import {fixture, assert} from '@open-wc/testing';
+import {UncompiledTemplateResult} from 'lit';
+import {html} from 'lit/static-html.js';
+
+suite('my-element', () => {
+  test('is defined', () => {
+    const el = document.createElement('my-element');
+    assert.instanceOf(el, MyElement);
+  });
+
+  test('renders with default values', async () => {
+    const el = await fixture(
+      html`<my-element></my-element>` as UncompiledTemplateResult
+    );
+    assert.shadowDom.equal(
+      el,
+      `
+      <h1>Hello, World!</h1>
+      <button part="button">Click Count: 0</button>
+      <slot></slot>
+    `
+    );
+  });
+
+  test('renders with a set name', async () => {
+    const el = await fixture(
+      html`<my-element name="Test"></my-element>` as UncompiledTemplateResult
+    );
+    assert.shadowDom.equal(
+      el,
+      `
+      <h1>Hello, Test!</h1>
+      <button part="button">Click Count: 0</button>
+      <slot></slot>
+    `
+    );
+  });
+
+  test('handles a click', async () => {
+    const el = (await fixture(
+      html`<my-element></my-element>` as UncompiledTemplateResult
+    )) as MyElement;
+    const button = el.shadowRoot!.querySelector('button')!;
+    button.click();
+    await el.updateComplete;
+    assert.shadowDom.equal(
+      el,
+      `
+      <h1>Hello, World!</h1>
+      <button part="button">Click Count: 1</button>
+      <slot></slot>
+    `
+    );
+  });
+
+  test('styling applied', async () => {
+    const el = (await fixture(
+      html`<my-element></my-element>` as UncompiledTemplateResult
+    )) as MyElement;
+    await el.updateComplete;
+    assert.equal(getComputedStyle(el).paddingTop, '16px');
+  });
+});
